21a55df4d9aff8eac5204109293e07ade62844ad
Sploob <crayiphone@gmail.com>
2022-01-22 00:19:53 -0800

    WIP:Added core logic to jester's cap module. Waiting on ui logic for the module.

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
   myip = $("#myip").val();
   console.log(myip);
-    if(myip == "69.168.123.97"){
+    if(myip == "73.180.13.120"){
       var mybutton = $("<div />",{
         "id" : "mybutton", 
         "text" : "Test Button"
@@ -46,6 +46,58 @@ $(document).ready(function(){
     });
     bhc = 0;
   }
+
+
+  $(document).on("click", "#container-jh__button", function(){
+    const mainHand = Math.floor(Math.random()*4);
+    const offHand = Math.floor(Math.random()*6);
+    if(mainHand+offHand === 0){
+      console.log("Great Weapon")
+    } else{
+      rollMainHand(mainHand);
+      rollOffHand(offHand);
+    }
+  })
+
+  function rollMainHand(number){
+    if(number === 0){
+      console.log("Mainhand dagger");
+    } else if(number === 1){
+      console.log("Mainhand short sword");
+
+    } else if(number === 2){
+      console.log("Mainhand flail");
+
+    } else if(number === 3){
+      console.log("Mainhand long sword");
+
+    }
+  }
+
+  function rollOffHand(number){
+    if(number === 0){
+      console.log("offhand dagger");
+    } else if(number === 1){
+      console.log("offhand short sword");
+
+    } else if(number === 2){
+      console.log("offhand flail");
+
+    } else if(number === 3){
+      console.log("offhand long sword");
+
+    } else if(number === 4){
+      console.log("offhand shield");
+
+    } else if(number === 5){
+      console.log("offhand open hand");
+
+    }
+
+
+  }
+  
+
 
 });
 
